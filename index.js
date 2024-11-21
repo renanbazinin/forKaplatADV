c3514109aedc32fc0a3e43cf11ed48a291c3d401
renan bazinin <renanbazinin2@gmail.com>
2024-11-21 20:11:29 +0200

    fix port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8574;
+const port = process.env.PORT;
 
 const { bookExists, validateBookData, getFilteredBooks, getBookById, validateGenres, books } = require('./helpers/bookHelpers');
 let { nextId } = require('./helpers/bookHelpers'); 
